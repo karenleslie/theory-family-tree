d5c77548b66e43a59015a909e4d088f84b6c7f99
karenleslie <karenlboyd@gmail.com>
2017-03-12 20:46:35 -0400

    Update colorcodedlinkeddata.js

--- colorcodedlinkeddata.js
+++ colorcodedlinkeddata.js
@@ -17,7 +17,7 @@ function parseCsv(data){
 //  console.log ("doop"+csv.toString());
 
 //split csv into array of triples
-  var tripleArray=csv.split('\r\n');
+  var tripleArray=csv.split('\h');
   // console.log (tripleArray[0]);
 
       var 
